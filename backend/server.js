93b9b589e7909d3c3dd1d12b7d2bfea318130c10
Samuli Laaksonen <samulilaaksonen1@gmail.com>
2023-05-03 17:47:54 +0300

    First backend test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
 
-let database = []
+let database = require("./testdata.json")
 
+
+app.get('/', (req, res) => {
+    res.status(200).json({database})
+})
+
+app.get('/:id', (req, res) => {
+    res.status(200).json(database.id)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Running in port ${PORT}`);
+})
